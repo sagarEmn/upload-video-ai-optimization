56f72134fc724d9caf0763da21fdd0f38dd34693
Sagar <desiferchef@gmail.com>
2025-06-26 17:01:53 +0545

    docs: Add comment for explanation

--- models/User.ts
+++ models/User.ts
@@ -19,11 +19,12 @@ const userSchema = new Schema<IUser>(
   }
 );
 
+// function to run before Mongoose starts save operation
 userSchema.pre("save", async function (next) {
   if (this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 10);
   }
-  next();
+  next(); // continue the saving operation 
 });
 
 const User = models?.User || model<IUser>("User", userSchema);
